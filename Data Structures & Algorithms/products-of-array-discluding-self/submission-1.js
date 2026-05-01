0658d4664af9249ac63098b403019630bcc7300c
MD OZAIR QAYAM <66690229+mdozairq@users.noreply.github.com>
2026-05-01 12:10:57 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let output = new Array(nums.length);
+        let product = 1;
+        let countZero = 0;
+        let zeroIndex = -1;
+        let zeroProduct = 1;
+
+        for (const n of nums) {
+            if (n == 0) {
+                countZero++;
+            }
+            product *= n;
+        }
+        if (countZero == 1) {
+            for (let i = 0; i < nums.length; i++) {
+                if (nums[i] == 0) {
+                    zeroIndex = i;
+                    continue;
+                }
+                zeroProduct *= nums[i];
+            }
+        }
+        console.log(countZero)
+        for (let i = 0; i < nums.length; i++) {
+            if(countZero == 1 && nums[i] == 0){
+                output[i] = zeroProduct;
+                continue;
+            }else if(countZero > 1){
+                output[i] = 0;
+            }else{
+            output[i] = product / nums[i];
+            }
+        }
+
+        return output;
+    }
+}
