45cfee95807cfa7f31c1c7ad832b05757498a10e
MD OZAIR QAYAM <66690229+mdozairq@users.noreply.github.com>
2026-05-10 15:23:42 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,43 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.min = Infinity;
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        if (this.stack.length === 0) {
+            this.stack.push(0);
+            this.min = val;
+        } else {
+            this.stack.push(val - this.min);
+            if (val < this.min) this.min = val;
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        let number = this.stack.pop();   
+        if (number < 0) this.min -= number;  
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        const top = this.stack[this.stack.length - 1];
+        return top > 0 ? top + this.min : this.min;
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+         return this.min;
+    }
+}
