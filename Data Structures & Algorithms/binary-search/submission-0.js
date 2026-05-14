5db4a9108952f03bc26d4207d7de467bc5cd9a61
MD OZAIR QAYAM <66690229+mdozairq@users.noreply.github.com>
2026-05-14 16:53:12 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let i = 0;
+        let j = nums.length-1;
+        while(i<=j){
+            let mid = i+Math.floor((j-i)/2);
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] > target){
+                j = mid-1;
+            }else{
+                i = mid+1;
+            }
+        }
+        return -1;
+    }
+}
