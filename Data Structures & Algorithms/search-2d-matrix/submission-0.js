baaacf4217a6624d49f5fef6b33cc14001702330
MD OZAIR QAYAM <66690229+mdozairq@users.noreply.github.com>
2026-05-18 21:07:51 +0530

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        let m = matrix.length;
+        let n = matrix[0].length;
+        let r = 0,
+            c = n - 1;
+
+        while (r < m && c >= 0) {
+            if (matrix[r][c] > target) {
+                c--;
+            } else if (matrix[r][c] < target) {
+                r++;
+            } else {
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
